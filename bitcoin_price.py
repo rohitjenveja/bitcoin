8a5c71ec2658abda0ac789233bd1368426970067
rohitjenveja <rjenveja@gmail.com>
2014-06-12 18:51:45 -0700

    Modified the module doc string and removed a few line breaks.

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python
 
-"""Get the current price of Bitcoin."""
+"""Get the current global average price of Bitcoin in USD."""
 
 
 import simplejson
 import urllib
 
-
 URL = 'https://api.bitcoinaverage.com/all'
 CURRENCY = 'USD'
 
-
 def GetData():
   return simplejson.load(urllib.urlopen(URL))
-  
   
 if __name__ == '__main__':
   data = GetData()
   with open('data', 'a') as f:
     f.write(str(data['USD']['global_averages']['last']) + '\n')
-  
   print data['USD']['global_averages']['last']
